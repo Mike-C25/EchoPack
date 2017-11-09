28d262d61572390db2c26ed4695fcdbd98e3e7cc
Michael Chiang <michael.chiang25@gmail.com>
2017-11-08 20:36:52 -0500

    added dependencies/server.js

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+var sassMiddleware = require('node-sass-middleware');
+var path = require('path');
+var app = express();
+var exphbs = require('express-handlebars');
+
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+
+// Set up view w. Handlebars
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Middleware
+// app.use(session({
+//     secret: 'app',
+//     cookie: { maxAge: 6 * 1000 * 1000 * 1000 * 1000 },
+//     resave: true,
+//     saveUninitialized: true,
+// }));
+app.use(cookieParser());
+
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.use(sassMiddleware({
+    /* Options */
+    src: __dirname,
+    dest: path.join(__dirname, 'public/assets/scss'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/public/assets/css' // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
+// Note: you must place sass-middleware *before* `express.static` or else it will
+// not work.
+
+
+// Models.sequelize.sync({ force: false }).then(function() {
+app.listen(PORT, server_host, function() {
+    console.log(`Listening on PORT: ${PORT}`);
+});
+// });
