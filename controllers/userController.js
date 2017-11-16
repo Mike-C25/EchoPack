8ee1e1f46829edc0fc7f15acd181a957ce345d94
vin <vrp908@gmail.com>
2017-11-16 15:03:30 -0500

    added passport/ other dependencies

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,39 +1,52 @@
 // Grabbing our models
 
 var db = require("../models"); // May need to specify .js file
+var passport = require("../config/passport"); // configured passport 
 
 // Routes
 // =============================================================
 
 module.exports = function(app) {
 
+
+// The following is the post route for authentication. 
+
+  app.post('/api/login', 
+    passport.authenticate('local'), function(req,res) {
+       res.json("/");
+    });
+
+ 
+//##################################################################################################
 // GET route for getting all of the users or specific one
-  app.get("/api/:User?", function(req, res) {
-    if (req.params.User){
-      // findAll returns all entries for a table when used with no options
-      db.User.findOne({
-        where: {
-          userName: req.params.userName,
-          password: req.params.pass
-        }
-      }).then(function(dbUser) {
-        // We have access to the Useres as an argument inside of the callback function
-        res.json(dbUser);
-      });
+// ############### COMMENTING OUT FOR NOW AS PASSPORT AS REPLACED THE NEED FOR THIS BELOW ########
+  // app.get("/api/:User?", function(req, res) {
+  //   if (req.params.User){
+  //     // findAll returns all entries for a table when used with no options
+  //     db.User.findOne({
+  //       where: {
+  //         userName: req.params.userName,
+  //         password: req.params.pass
+  //       }
+  //     }).then(function(dbUser) {
+  //       // We have access to the Useres as an argument inside of the callback function
+  //       res.json(dbUser);
+  //     });
 
-    }
+  //   }
 
-    else{
-      // findAll returns all entries for a table when used with no options
-      db.User.findAll({
-        // limit: 3,
-        // order: [[sequelize.col('score'), 'DESC']]
-      }).then(function(dbUser) {
-        // We have access to the forums as an argument inside of the callback function
-        res.json(dbUser);
-      });
-    }
-  });
+  //   else{
+  //     // findAll returns all entries for a table when used with no options
+  //     db.User.findAll({
+  //       // limit: 3,
+  //       // order: [[sequelize.col('score'), 'DESC']]
+  //     }).then(function(dbUser) {
+  //       // We have access to the forums as an argument inside of the callback function
+  //       res.json(dbUser);
+  //     });
+  //   }
+  // });
+// ###############################################################################################
 
 //
 // POST route for saving a new User
--- server.js
+++ server.js
@@ -7,10 +7,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+var session = require("express-session");
 // const sassMiddleware = require('node-sass-middleware')
 const path = require('path');
 const exphbs = require('express-handlebars');
+const passport = require('./config/passport'); // our local passport strategy
 const app = express();
+
 const home = require('./controllers/homeController.js');
 const box = require('./controllers/boxController.js');
 const post = require('./controllers/postController.js');
@@ -41,8 +44,12 @@ app.set('view engine', 'handlebars');
 // Note: you must place sass-middleware *before* `express.static` or else it will 
 // not work. 
 app.use('/public', express.static(path.join(__dirname, 'public')));
+// Vin added this... not sure if we need ?
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
-
+app.use(passport.initialize());
+app.use(passport.session());
 //Use Controllers
 app.use('/', home);
 // home(app);
@@ -61,6 +68,6 @@ app.listen(PORT, server_host, function() {
 */
 db.sequelize.sync({force:false}).then(function(){
 	app.listen(PORT, function(){
-		console.log(`Listening on PORT: ${PORT}`);
+		console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
 	})
 })
