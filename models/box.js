8146bd58b603f8ee8b8a4ca1accf4ebee1e518db
Michael Chiang <michael.chiang25@gmail.com>
2017-11-15 19:37:15 -0500

    create forum ajax

--- models/box.js
+++ models/box.js
@@ -1,46 +1,46 @@
 module.exports = function(sequelize, DataTypes) {
-  var Box = sequelize.define("Box", {
-    id: { 
-      type: DataTypes.INTEGER, 
-      primaryKey: true,
-      autoIncrement: true
-    },
-    authorUserId: DataTypes.INTEGER,
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      len: [1]
-    },
-    sentimentScore: DataTypes.INTEGER,
-    Date: DataTypes.DATE
-  },
-  {
-   freezeTableName: true // needed?
-  });
+        var Box = sequelize.define("Box", {
+                id: {
+                    type: DataTypes.INTEGER,
+                    primaryKey: true,
+                    autoIncrement: true
+                },
+                authorUserId: DataTypes.INTEGER,
+                title: {
+                    type: DataTypes.STRING,
+                    allowNull: false,
+                    validate: {
+                        len: [1]
+                    },
+                    description: {
+                        type: DataTypes.TEXT,
+                        allowNull: false,
+                        len: [1]
+                    },
+                    sentimentScore: DataTypes.INTEGER,
+                    Date: DataTypes.DATE
+                },
+                {
+                    freezeTableName: true // needed?
+                });
 
-  Box.associate = function(models) {
-    // We're saying that a Box should belong to an User
-    // A Box can't be created without an user due to the foreign key constraint
-    Box.belongsTo(models.User, {
-      foreignKey: {
-        allowNull: false
-      }
-    });
-  };
+            Box.associate = function(models) {
+                // We're saying that a Box should belong to an User
+                // A Box can't be created without an user due to the foreign key constraint
+                Box.belongsTo(models.User, {
+                    foreignKey: {
+                        allowNull: false
+                    }
+                });
+            };
 
-   Box.associate = function(models) {
-      // Associating Box with Posts
-      // When an Box is deleted, also delete any associated Posts
-      Box.hasMany(models.Post, {
-        onDelete: "cascade"
-      });
-    };
+            Box.associate = function(models) {
+                // Associating Box with Posts
+                // When an Box is deleted, also delete any associated Posts
+                Box.hasMany(models.Post, {
+                    onDelete: "cascade"
+                });
+            };
 
-  return Box;
-};
+            return Box;
+        };
