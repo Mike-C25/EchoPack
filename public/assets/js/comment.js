17610047477f228b76fb79d30d93dbd2d1f6d654
Michael Chiang <michael.chiang25@gmail.com>
2017-11-17 21:58:13 -0500

    minor comment.js changes

--- public/assets/js/comment.js
+++ public/assets/js/comment.js
@@ -4,6 +4,11 @@ $(document).ready(function() {
 
     $('.postc-submit').on('click', function(e) {
        let comment = $('.postb-text textarea').val().trim();
+       let sub = window.location.href.indexOf("?=id")
+       console.log(sub);
+       let fID = window.location.href.substring(sub)
+       console.log(fID);
+
     });	
 
 });    	
